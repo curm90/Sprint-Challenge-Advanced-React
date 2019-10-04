1497a9dfa38147d91c088978d8cabf112f173dca
Liam Sutton <devliam90@gmail.com>
2019-10-04 13:48:03 +0100

    fix render players test

--- client/src/components/__tests__/PlayerList.test.js
+++ client/src/components/__tests__/PlayerList.test.js
@@ -11,14 +11,11 @@ describe('Playerlist component', () => {
     expect(getByText(/loading.../i)).toBeInTheDocument();
   });
 
-  // Doesnt work yet :(
   it('renders players', () => {
-    const fakePlayers = [{ id: 1, name: 'Alex', id: 2, name: 'Megan' }];
+    const fakePlayers = [{ id: 1, name: 'Alex' }, { id: 2, name: 'Megan' }];
     const { getAllByTestId } = render(<PlayerList players={fakePlayers} />);
-    const playerNames = getAllByTestId('player-name').map(
-      div => div.textContent
-    );
-    const fakplayernames = fakePlayers.map(player => player.name);
-    expect(playerNames).toEqual(fakplayernames);
+    const playerNames = getAllByTestId('player-name').map(li => li.textContent);
+    const fakePlayernames = fakePlayers.map(player => `Name: ${player.name}`);
+    expect(fakePlayernames).toEqual(playerNames);
   });
 });
